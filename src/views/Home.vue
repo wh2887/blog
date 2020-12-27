83e2806882b4614d7b7be19e19de1cbd582cc8f3
McCallWang <13567272790@163.com>
2020-12-27 15:33:58 +0800

    引入 element-ui 并配置按需加载

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     Home
+    <el-button type="success">按钮</el-button>
   </div>
 </template>
 
@@ -8,6 +9,9 @@
 import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
 import Nav from '@/components/Nav.vue';
+import {Button} from 'element-ui';
+
+Vue.use(Button);
 
 @Component({
   components: {Nav}
